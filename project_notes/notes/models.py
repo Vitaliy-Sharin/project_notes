79f2dfb8dce73f97859ab71e3831f8826fc668bc
Виталий <vitsharin11@gmail.com>
2024-12-23 11:11:57 +0300

    Добавление модели Папки и настройка проекта

--- project_notes/notes/models.py
+++ project_notes/notes/models.py
@@ -3,12 +3,22 @@ from django.db import models
 
 
 # Create your models here.
-class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Пользователь')
-    name = models.CharField(max_length=300, verbose_name='Название заметки')
-    note_text = models.TextField(max_length=20000, verbose_name='Текст заметки')
+class Folder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    name = models.CharField(max_length=300, verbose_name='Название папки')
     date_create = models.DateTimeField(verbose_name='Дата создания')
-    date_of_editing = models.DateTimeField(verbose='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}/{self.user.name}'
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    name = models.CharField(max_length=300, verbose_name='Название заметки')
+    note_text = models.TextField(max_length=50000, verbose_name='Текст заметки')
+    date_create = models.DateTimeField(verbose_name='Дата создания')
+    date_of_editing = models.DateTimeField(verbose_name='Дата последнего изменения')
+    parent_folder = models.ForeignKey(Folder, on_delete=models.CASCADE, verbose_name='Родительская папка')
 
     def __str__(self):
         return f'{self.name}/{self.user.name}'
--- project_notes/project_notes/settings.py
+++ project_notes/project_notes/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project_notes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
