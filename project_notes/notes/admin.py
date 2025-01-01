44107c7ffc422d27547ebadfd1baff1bcd5b2acb
Виталий <vitsharin11@gmail.com>
2025-01-01 18:58:00 +0300

    Доработка таблиц Note и Folder, добавление значения по умолчанию для родительской папки у заметки

--- project_notes/notes/admin.py
+++ project_notes/notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from notes.models import Folder, Note
+
 # Register your models here.
+admin.site.register(Folder)
+admin.site.register(Note)
--- project_notes/notes/models.py
+++ project_notes/notes/models.py
@@ -8,8 +8,13 @@ class Folder(models.Model):
     name = models.CharField(max_length=300, verbose_name='Название папки')
     date_create = models.DateTimeField(verbose_name='Дата создания')
 
+    class Meta:
+        verbose_name = 'Папка'
+        verbose_name_plural = 'Папки'
+        ordering = ['user', 'name']
+
     def __str__(self):
-        return f'{self.name}/{self.user.name}'
+        return f'{self.name}/{str(self.user)}'
 
 
 class Note(models.Model):
@@ -18,7 +23,13 @@ class Note(models.Model):
     note_text = models.TextField(max_length=50000, verbose_name='Текст заметки')
     date_create = models.DateTimeField(verbose_name='Дата создания')
     date_of_editing = models.DateTimeField(verbose_name='Дата последнего изменения')
-    parent_folder = models.ForeignKey(Folder, on_delete=models.CASCADE, verbose_name='Родительская папка')
+    parent_folder = models.ForeignKey(Folder, on_delete=models.CASCADE, verbose_name='Родительская папка', default=None,
+                                      null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
+        ordering = ['user', 'name']
 
     def __str__(self):
-        return f'{self.name}/{self.user.name}'
+        return f'{self.name}/{str(self.user)}'
