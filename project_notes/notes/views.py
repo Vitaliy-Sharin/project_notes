c7f04b009732898b17a41920456343203cb51af0
Виталий <vitsharin11@gmail.com>
2025-01-14 10:12:42 +0300

    Добавление домашней страницы и базового шаблона

--- project_notes/notes/views.py
+++ project_notes/notes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from notes.models import Note, Folder
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    user = request.user
+    user_notes = []
+    user_folders = []
+    if user:
+        user_notes = Note.objects.filter(user=user.id, parent_folder=None)
+        user_folders = Folder.objects.filter(user=user.id)
+    return render(request, 'home.html', {'user_notes': user_notes, 'user_folders': user_folders})
+
