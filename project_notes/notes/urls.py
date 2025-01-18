125f49dd6a5a35dc912b5342348c98fb9c4d69c3
Виталий <vitsharin11@gmail.com>
2025-01-18 15:10:18 +0300

    Добавление html шаблона просмотра папки с заметками

--- project_notes/notes/urls.py
+++ project_notes/notes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user/<int:id_user>/view_folder/<int:id_folder>', views.view_folder, name='view_folder'),
 ]
--- project_notes/notes/views.py
+++ project_notes/notes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 
 from notes.models import Note, Folder
 
@@ -13,3 +15,19 @@ def home(request):
         user_folders = Folder.objects.filter(user=user.id)
     return render(request, 'home.html', {'user_notes': user_notes, 'user_folders': user_folders})
 
+
+@login_required
+@require_http_methods(['GET'])
+def view_folder(request, id_user, id_folder):
+    user = request.user
+    if user.id != id_user:
+        user_error = 'Данная папка принадлежит другому пользователю'
+        return render(request, 'view_folder.html', {'user_error': user_error, 'folder': None, 'notes': None})
+    getted_folder = get_object_or_404(Folder, id=id_folder, user__id=id_user)
+    folder_notes = Note.objects.filter(user=user.id, parent_folder__id=id_folder)
+    data = {
+        'user_error': None,
+        'folder': getted_folder,
+        'notes': folder_notes
+        }
+    return render(request, 'view_folder.html', data)
