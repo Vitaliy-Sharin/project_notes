d3b6e66bd4b6dcd7494a701d911d943ff1d3421c
Виталий <vitsharin11@gmail.com>
2025-02-06 20:38:12 +0300

    Добавлена возможность просматривать заметки пользователя

--- project_notes/notes/urls.py
+++ project_notes/notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('user/<int:id_user>/view_folder/<int:id_folder>', views.view_folder, name='view_folder'),
+    path('user/<int:id_user>/view_note/<int:id_note>', views.view_note, name='view_note'),
 ]
--- project_notes/notes/views.py
+++ project_notes/notes/views.py
@@ -6,6 +6,7 @@ from notes.models import Note, Folder
 
 
 # Create your views here.
+@require_http_methods(['GET'])
 def home(request):
     user = request.user
     user_notes = []
@@ -23,11 +24,26 @@ def view_folder(request, id_user, id_folder):
     if user.id != id_user:
         user_error = 'Данная папка принадлежит другому пользователю'
         return render(request, 'view_folder.html', {'user_error': user_error, 'folder': None, 'notes': None})
-    getted_folder = get_object_or_404(Folder, id=id_folder, user__id=id_user)
+    gotten_folder = get_object_or_404(Folder, id=id_folder, user__id=id_user)
     folder_notes = Note.objects.filter(user=user.id, parent_folder__id=id_folder)
     data = {
         'user_error': None,
-        'folder': getted_folder,
+        'folder': gotten_folder,
         'notes': folder_notes
-        }
+    }
     return render(request, 'view_folder.html', data)
+
+
+@login_required
+@require_http_methods(['GET'])
+def view_note(request, id_user, id_note):
+    user = request.user
+    if user.id != id_user:
+        user_error = 'Данная заметка принадлежит другому пользователю'
+        return render(request, 'view_user_note.html', {'user_error': user_error})
+    gotten_note = get_object_or_404(Note, id=id_note, user__id=id_user)
+    data = {
+        'user_error': None,
+        'note': gotten_note,
+    }
+    return render(request, 'view_user_note.html', data)
